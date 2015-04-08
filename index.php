f7f3f782fd1247573eb6e0a8697b9abbe9018022
seedbornmuse <priest.of.titania@gmail.com>
2015-04-08 15:26:55 -0700

    update

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+function login($username, $password, $db){
+}
+
+$connection = new Mongo();
+
+$db = $connection->selectDB('cubedb');
+
+$collection = $db->users;
+
+$doc_cursor = $collection->find();
+
+foreach ($doc_cursor as $doc) {
+    foreach ($doc as $key => $val){
+        echo $key . ': ' . $val . '<br>';
+}
+}
+
+?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+
+<div id="login">
+	<form method="POST" action="index.php">
+		Username: <input type="text" name="username">
+		Password: <input type="password" name="password">
+		<input type="submit" value="Submit">
+	</form>
+
+</div>
+
+</body>
+</html>
